f8796e8ac3661912a393bacad7634622bb78c254
antoine <antoine.stuff@pm.me>
2020-12-08 21:00:30 +0100

    update terminal messages

--- asb-test.py
+++ asb-test.py
@@ -53,7 +53,7 @@ if 'sensors_enabled' in globals():
 # Getting metrics
 if len(these_sensors) > 0:
     these_sensors_metrics = kit.readSensors(
-        sensors=these_sensors, iter_num=5, delay=0.1, unit='V', method='avg')
+        sensors=these_sensors, iter_num=2, delay=0.1, unit='V', method='avg')
 
 if 'kit_id' in globals() and 'these_sensors_metrics' in globals():
 
@@ -69,6 +69,11 @@ if 'kit_id' in globals() and 'these_sensors_metrics' in globals():
     this_kit_formatted[kit_id] = dict(
         zip(new_keys, list(this_kit[kit_id].values())))
 
+    # From V to mV, rounded
+    for item in this_kit_formatted[kit_id]:
+        this_kit_formatted[kit_id][item] = round(
+            this_kit_formatted[kit_id][item] * 1000, 2)
+
 # Getting ID's from hardware.json
 if 'data_hardware' in globals() and 'kit_id' in globals():
     hardware_ids = data_hardware[list(data_hardware.keys())[1]]['1']['ids']
@@ -82,14 +87,18 @@ if 'data_calibrations' in globals() and 'hardware_ids' in globals():
         try:
             calibrations_values[i] = data_calibrations[i]
             if (data_index == 1):
-                calibrations_values[i]['ae_electronic_zero_mv'] = str(this_kit_formatted[kit_id].get('Ch0'))
-                calibrations_values[i]['we_electronic_zero_mv'] = str(this_kit_formatted[kit_id].get('Ch1'))
+                calibrations_values[i]['ae_electronic_zero_mv'] = str(
+                    this_kit_formatted[kit_id].get('Ch0'))
+                calibrations_values[i]['we_electronic_zero_mv'] = str(
+                    this_kit_formatted[kit_id].get('Ch1'))
             elif (data_index == 2):
-                calibrations_values[i]['ae_electronic_zero_mv'] = str(this_kit_formatted[kit_id].get('Ch2'))
-                calibrations_values[i]['we_electronic_zero_mv'] = str(this_kit_formatted[kit_id].get('Ch3'))
+                calibrations_values[i]['ae_electronic_zero_mv'] = str(
+                    this_kit_formatted[kit_id].get('Ch2'))
+                calibrations_values[i]['we_electronic_zero_mv'] = str(
+                    this_kit_formatted[kit_id].get('Ch3'))
         except:
             print('Sensor ID', i, 'does not exist')
-    
+
     # Update the calibrations file
     data_calibrations.update(calibrations_values)
     with open(file_calibrations, 'w') as file:
