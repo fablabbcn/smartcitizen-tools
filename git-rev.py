4bd4446bfb97a889cf338d9f10ac5c26d896dbf5
Xavi <xavi.vinaixa@gmail.com>
2020-12-20 09:11:19 +0100

    add time to ISO_DATE

--- git-rev.py
+++ git-rev.py
@@ -7,7 +7,7 @@ revision = (
     .decode("utf-8")
 )
 
-iso_date = datetime.date.today().isoformat()
+iso_date = datetime.datetime.utcnow().replace(microsecond=0).isoformat()+'Z'
 
 print("-D__GIT_HASH__='\"%s\"'" % revision)
 print("-D__ISO_DATE__='\"%s\"'" % iso_date)
