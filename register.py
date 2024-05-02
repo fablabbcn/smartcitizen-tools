ff66911e6f99108fbddbb24265e4c3e066496279
oscgonfer <oscgonfer@gmail.com>
2024-05-02 10:58:58 +0200

    Remove deliveries inventory

--- register.py
+++ register.py
@@ -85,12 +85,7 @@ if 'register' in sys.argv:
     print("Postprocessing['meta']: " + kit.postprocessing_meta)
 
 if 'inventory' in sys.argv:
-    try:
-        from secret import inventory_path
-    except:
-        print ('No inventory path defined, using inventory/ folder')
-        inventory_path = os.path.join(os.path.realpath(os.path.dirname(__file__)), 'inventory')
-        pass
+    inventory_path = os.path.join(os.path.realpath(os.path.dirname(__file__)), 'inventory')
 
     print (f'Using inventory in: {inventory_path}')
     kit.description = sys.argv[sys.argv.index('-d')+1]
@@ -108,47 +103,18 @@ if 'inventory' in sys.argv:
     if not hasattr(kit, 'platform_url'):
         kit.platform_url = ''
 
-    s3_inv_path = "inventory/deliveries"
     local_inv_name = "inventory.csv"
     if not os.path.exists(inventory_path): os.makedirs(inventory_path)
 
-    sync = None
-    try:
-        from boto.s3.connection import S3Connection
-    except ModuleNotFoundError:
-        boto_avail = False
-        pass
-    else:
-        boto_avail = True
-
-    if boto_avail:
-        # Try to download file from S3
-        sync = S3handler()
-        sync.download(os.path.join(inventory_path, local_inv_name), os.path.join(s3_inv_path, local_inv_name))
-        # Open the file
-        print ('File from S3 synced correctly')
+    if os.path.exists(os.path.join(inventory_path, local_inv_name)):
+        shutil.copyfile(os.path.join(inventory_path, local_inv_name), inventory_path+".BAK")
         csvFile = open(os.path.join(inventory_path, local_inv_name), "a")
     else:
-        # Keep things local
-        print_exc()
-        print('Problem downloading file from S3, using local file')
-
-        if os.path.exists(os.path.join(inventory_path, local_inv_name)):
-            shutil.copyfile(os.path.join(inventory_path, local_inv_name), inventory_path+".BAK")
-            csvFile = open(os.path.join(inventory_path, local_inv_name), "a")
-        else:
-            csvFile = open(os.path.join(inventory_path, local_inv_name), "w")
-            csvFile.write("time,serial,mac,sam_firmVer,esp_firmVer,description,token,platform_name,platform_url,tested,validated,min_validation_date,max_validation_date,replacement,test,destination,batch\n")
-        pass
+        csvFile = open(os.path.join(inventory_path, local_inv_name), "w")
+        csvFile.write("time,serial,mac,sam_firmVer,esp_firmVer,description,token,platform_name,platform_url,tested,validated,min_validation_date,max_validation_date,replacement,test,destination,batch\n")
+    pass
 
     print (f'Writing into file for Kit: {kit.esp_macAddress}')
     csvFile.write(time.strftime("%Y-%m-%dT%H:%M:%SZ,", time.gmtime()))
     csvFile.write(kit.sam_serialNum + ',' + kit.esp_macAddress + ',' + kit.sam_firmVer + ',' + kit.esp_firmVer + ',' + kit.description + ',' + kit.token + ',' + kit.platform_name + ',' + kit.platform_url + ',' + tested + ',' + ',' + ',' +',' + ',' +',' + ',' +'\n')
     csvFile.close()
-
-    # Put the file in S3
-    if sync is not None:
-        resp = sync.upload(os.path.join(inventory_path, local_inv_name), os.path.join(s3_inv_path, local_inv_name))
-
-        if resp is None: print ('No response, review bucket')
-        else: print ('Success!')
