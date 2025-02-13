f9f82464cf8f8502c80be82da82cda97b9448201
oscgonfer <oscgonfer@gmail.com>
2025-02-13 13:06:19 +0100

    Monitor script

---
+++ monitor.py
@@ -0,0 +1,29 @@
+import sys
+# sys.path.append("./tools")
+from sck import sck
+
+if __name__ == '__main__':
+
+    print ('Opening kit')
+    kit = sck(to_register=True)
+    kit.begin(port="/dev/ttyACM0")
+
+    print ('Getting sensors')
+    kit.getSensors()
+    print (kit.sensor_enabled)
+
+    kit.monitor(sensors=['Temperature', 'Humidity', 'Noise dBA'])
+
+    print ('Getting the worker output')
+    while True:
+        data = kit.worker.output.get()
+        print (data[0])
+        print (data[1])
+
+        # Do whatever with the data
+
+
+
+
+
+
